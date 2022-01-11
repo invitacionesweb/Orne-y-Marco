b1e2badc0202c8c8d3eecfd16b2959914e064f94
Ara Garrido <94240265+invitacionesweb@users.noreply.github.com>
2022-01-11 17:32:57 -0300

    baja duracion

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ const parallax = document.getElementById ("parallax");
 window.addEventListener("scroll", function ()
 {
     let offset = this.window.pageYOffset; 
-    parallax.style.backgroundPositionY = offset * 0.7 + "px";
+    parallax.style.backgroundPositionY = offset * 0.2 + "px";
 })
 
